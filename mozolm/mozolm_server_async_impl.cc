bd88bbad8dd12f75f32659b402b78ec4c1658c26
Alexander Gutkin <agutkin@google.com>
2020-11-20 19:07:36 +0000

    License header changes.
    PiperOrigin-RevId: 343526545

--- mozolm/mozolm_server_async_impl.cc
+++ mozolm/mozolm_server_async_impl.cc
@@ -12,19 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include "mozolm/mozolm_server_async_impl.h"
 
 #include <string>
 
@@ -34,7 +22,6 @@
 #include "include/grpcpp/server_builder.h"
 #include "mozolm/mozolm_model.h"
 #include "mozolm/mozolm_bigram_char_model.h"
-#include "mozolm/mozolm_server_async_impl.h"
 #include "mozolm/lm_scores.grpc.pb.h"
 #include "mozolm/lm_scores.pb.h"
 
