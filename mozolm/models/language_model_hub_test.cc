fc5ede74211f77e767c679f07e84d0661b928416
Alexander Gutkin <agutkin@google.com>
2021-10-17 10:54:40 -0700

    Factored out buggy scenario into its own test case.
    PiperOrigin-RevId: 403780384

--- mozolm/models/language_model_hub_test.cc
+++ mozolm/models/language_model_hub_test.cc
@@ -39,40 +39,55 @@ namespace models {
 namespace {
 
 // Name of a temporary vocabulary file.
-const char kVocabFileName[] = "vocab.txt";
+constexpr char kVocabFileName[] = "vocab.txt";
 
 // Epsilon for floating point comparisons.
-const double kEpsilon = 1E-3;
+constexpr double kEpsilon = 1E-3;
 
-TEST(LanguageModelHubTest, VocabOnlyPpmModel) {
-  // Create configuration for the PPM model.
-  ModelStorage storage;
-  const int max_order = 2;
-  storage.mutable_ppm_options()->set_max_order(max_order);
-  storage.mutable_ppm_options()->set_static_model(false);
-  const auto write_status = WriteTempTextFile(kVocabFileName, "ab");
-  ASSERT_OK(write_status.status());
-  const std::string vocab_path = write_status.value();
-  EXPECT_FALSE(vocab_path.empty());
-  storage.set_vocabulary_file(vocab_path);
+// ASCII for `a` and `b`.
+constexpr int kAsciiA = 97;
+constexpr int kAsciiB = 98;
 
-  // Create model hub config.
-  ModelHubConfig config;
-  ModelConfig *model_config = config.add_model_config();
-  model_config->set_type(ModelConfig::PPM_AS_FST);
-  *model_config->mutable_storage() = storage;
+class VocabOnlySingleModelTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    // Create configuration for the PPM model.
+    ModelStorage storage;
+    const int max_order = 2;
+    storage.mutable_ppm_options()->set_max_order(max_order);
+    storage.mutable_ppm_options()->set_static_model(false);
+    const auto write_status = WriteTempTextFile(kVocabFileName, "ab");
+    ASSERT_OK(write_status.status());
+    const std::string vocab_path = write_status.value();
+    EXPECT_FALSE(vocab_path.empty());
+    storage.set_vocabulary_file(vocab_path);
 
-  // Initialize the hub.
-  auto hub_status = MakeModelHub(config);
-  ASSERT_TRUE(hub_status.ok()) << "Failed to initialize model hub";
-  std::unique_ptr<LanguageModelHub> hub = std::move(hub_status.value());
-  EXPECT_TRUE(std::filesystem::remove(vocab_path));
+    // Create model hub config.
+    ModelConfig *model_config = config_.add_model_config();
+    model_config->set_type(ModelConfig::PPM_AS_FST);
+    *model_config->mutable_storage() = storage;
 
+    // Initialize the hub.
+    auto hub_status = MakeModelHub(config_);
+    ASSERT_TRUE(hub_status.ok()) << "Failed to initialize model hub";
+    hub_ = std::move(hub_status.value());
+    EXPECT_TRUE(std::filesystem::remove(vocab_path));
+
+    // Fetch start state.
+    start_state_ = hub_->ContextState("");
+    EXPECT_LE(0, start_state_);
+  }
+
+  ModelHubConfig config_;
+  std::unique_ptr<LanguageModelHub> hub_;
+  int start_state_;
+};
+
+TEST_F(VocabOnlySingleModelTest, NonUniformProbs) {
   // Retrieve initial estimates: These should correspond to the uniform
   // distribution over the three symbols.
   LMScores scores;
-  const int start_state = hub->ContextState("");
-  ASSERT_TRUE(hub->ExtractLMScores(start_state, &scores));
+  ASSERT_TRUE(hub_->ExtractLMScores(start_state_, &scores));
   ASSERT_EQ(3, scores.symbols_size());
   EXPECT_EQ("", scores.symbols(0));  // </S>.
   EXPECT_EQ("a", scores.symbols(1));
@@ -81,21 +96,40 @@ TEST(LanguageModelHubTest, VocabOnlyPpmModel) {
   EXPECT_THAT(scores.probabilities(), Each(DoubleEq(1.0 / 3)));
 
   // Update the model.
-  // TODO: Following is a bug. Can't update by count > 1, have
-  // to repeat the call `count` times instead.
-  EXPECT_TRUE(hub->UpdateLMCounts(
-      start_state, {97}, 1));  // Updates "a" count.
-  EXPECT_TRUE(hub->UpdateLMCounts(
-      start_state, {97}, 1));  // Updates "a" count.
-  EXPECT_TRUE(hub->UpdateLMCounts(
-      start_state, {98}, 1));  // Updates "b" count.
+  EXPECT_TRUE(hub_->UpdateLMCounts(start_state_, {kAsciiA}, 1));
+  EXPECT_TRUE(hub_->UpdateLMCounts(start_state_, {kAsciiA}, 1));
+  EXPECT_TRUE(hub_->UpdateLMCounts(start_state_, {kAsciiB}, 1));
 
   // Get new estimates.
-  ASSERT_TRUE(hub->ExtractLMScores(start_state, &scores));
+  ASSERT_TRUE(hub_->ExtractLMScores(start_state_, &scores));
   ASSERT_EQ(3, scores.probabilities_size());
   EXPECT_NEAR(scores.probabilities(0), 0.142857, kEpsilon);  // </S>
   EXPECT_NEAR(scores.probabilities(1), 0.571429, kEpsilon);  // "a"
   EXPECT_NEAR(scores.probabilities(2), 0.285714, kEpsilon);  // "b"
+}
+
+TEST_F(VocabOnlySingleModelTest, UpdateByNonSingletonCountBug) {
+  // TODO: Following is a bug. Can't update by count > 1, have
+  // to repeat the call `count` times instead.
+  EXPECT_FALSE(hub_->UpdateLMCounts(start_state_, {kAsciiA}, 2));
+}
+
+TEST_F(VocabOnlySingleModelTest, CheckNextStateAndStateSymbol) {
+  constexpr int kBadSymbol = 1234;
+  EXPECT_GT(0, hub_->StateSym(kBadSymbol));
+  // TODO: Following line exposes another bug. The call actually
+  // succeeds in adding a state, but it shouldn't.
+  // constexpr int kBadState = 100;
+  // EXPECT_LT(0, hub_->NextState(kBadState, kBadSymbol));
+
+  const int start_sym = hub_->StateSym(start_state_);
+  EXPECT_EQ(0, start_sym);  // "</S>"
+  EXPECT_EQ(1, hub_->NextState(start_state_, /* </S> */0));
+  EXPECT_EQ(2, hub_->NextState(start_state_, kAsciiA));
+  EXPECT_EQ(3, hub_->NextState(start_state_, kAsciiB));
+  EXPECT_EQ(4, hub_->NextState(1, /* </S> */0));
+  EXPECT_EQ(5, hub_->NextState(1, kAsciiA));
+  EXPECT_EQ(6, hub_->NextState(1, kAsciiB));
 }
 
 }  // namespace
